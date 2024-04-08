19bb807d1211540e527f61cb63a646c492ca3cc3
Steffen Karlsson <steffen.karlsson@maersk.com>
2024-04-08 13:58:37 +0200

    eslint

--- app/src/styled.d.ts
+++ app/src/styled.d.ts
@@ -1,5 +1,6 @@
-import 'styled-components';
 import { Theme } from '@mui/material';
+
+import 'styled-components';
 
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
