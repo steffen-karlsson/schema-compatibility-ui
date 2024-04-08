447ed91b0e69f441ea9b7bad3d032efdd763cd8d
Steffen Karlsson <steffen.karlsson@maersk.com>
2024-04-08 10:15:59 +0200

    Better map for enums

--- api/src/main/java/com/swk/sr/ui/factory/SchemaProviderFactory.java
+++ api/src/main/java/com/swk/sr/ui/factory/SchemaProviderFactory.java
@@ -3,7 +3,7 @@ package com.swk.sr.ui.factory;
 import com.swk.sr.ui.model.SchemaTypeDTO;
 import com.swk.sr.ui.service.AbstractCompatibilityService;
 import jakarta.annotation.PostConstruct;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.stereotype.Service;
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class SchemaProviderFactory {
 
-  private static final Map<SchemaTypeDTO, AbstractCompatibilityService<?>> CACHE = new HashMap<>();
+  private static final Map<SchemaTypeDTO, AbstractCompatibilityService<?>> CACHE = new EnumMap<>(SchemaTypeDTO.class);
   private final List<AbstractCompatibilityService<?>> services;
 
   public SchemaProviderFactory(List<AbstractCompatibilityService<?>> services) {
