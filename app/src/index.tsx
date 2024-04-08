946c06beac4c3b6624b3a31206691743b2feb65f
Steffen Karlsson <steffen.karlsson@maersk.com>
2024-04-08 12:53:56 +0200

    Removing default MUI padding on `body`

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -5,6 +5,8 @@ import { StyledEngineProvider } from '@mui/material/styles';
 import App from './components/App';
 import { ThemeContextProvider } from './components/contexts/ThemeContextProvider';
 
+import 'global.css';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeContextProvider>
