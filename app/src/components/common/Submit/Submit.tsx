c9b1eb87c8f58db046679f39eca31de5ba3b31dc
Steffen Karlsson <steffen.karlsson@maersk.com>
2024-04-10 09:27:41 +0200

    Using loading button instead

--- app/src/components/common/Submit/Submit.tsx
+++ app/src/components/common/Submit/Submit.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import Check from '@mui/icons-material/Check';
-import { Fab } from '@mui/material';
+import { Check } from '@mui/icons-material';
+import LoadingButton from '@mui/lab/LoadingButton';
 
 const Submit: React.FC = () => {
+  const [loading, setLoading] = React.useState(false);
+
+  const onClick = () => {
+    setLoading(true);
+  };
+
   return (
-    <Fab size="medium" variant="extended" color="success">
-      <Check />
+    <LoadingButton
+      size="large"
+      color="success"
+      loading={loading}
+      loadingPosition="start"
+      startIcon={<Check />}
+      variant="outlined"
+      onClick={onClick}
+    >
       Validate
-    </Fab>
+    </LoadingButton>
   );
 };
 
