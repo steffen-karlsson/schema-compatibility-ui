b0e730521d464ea7f1c753dd73d9e5a64f30d483
Steffen Wirenfeldt Karlsson <steffen.karlsson@maersk.com>
2024-05-23 09:35:53 +0200

    Better visibility validation on Validate button (#7)

--- app/src/components/common/Editor/Editor.tsx
+++ app/src/components/common/Editor/Editor.tsx
@@ -19,18 +19,30 @@ export interface EditorProps {
 const Editor: React.FC<EditorProps> = ({ height }) => {
   const theme = useTheme();
   const schemaType = localStorage.getItem(SCHEMA_TYPE) as SchemaType;
+  const [existing, setExisting] = React.useState<string>(
+    localStorage.getItem(SCHEMA_EXISTING)
+      ? atob(localStorage.getItem(SCHEMA_EXISTING) as string)
+      : ''
+  );
+  const [proposed, setProposed] = React.useState<string>(
+    localStorage.getItem(SCHEMA_PROPOSED)
+      ? atob(localStorage.getItem(SCHEMA_PROPOSED) as string)
+      : ''
+  );
 
-  const onEditorChange = (exiting: string, proposed: string) => {
-    if (exiting === '') {
+  const onEditorChange = (newExisting: string, newProposed: string) => {
+    setExisting(newExisting);
+    if (newExisting === '') {
       removeLocalStorage(SCHEMA_EXISTING);
     } else {
-      updateLocalStorage(SCHEMA_EXISTING, btoa(exiting));
+      updateLocalStorage(SCHEMA_EXISTING, btoa(newExisting));
     }
 
-    if (proposed === '') {
+    setProposed(newProposed);
+    if (newProposed === '') {
       removeLocalStorage(SCHEMA_PROPOSED);
     } else {
-      updateLocalStorage(SCHEMA_PROPOSED, btoa(proposed));
+      updateLocalStorage(SCHEMA_PROPOSED, btoa(newProposed));
     }
   };
 
@@ -51,16 +63,12 @@ const Editor: React.FC<EditorProps> = ({ height }) => {
       showPrintMargin={false}
       enableBasicAutocompletion
       orientation="beside"
-      value={[
-        localStorage.getItem(SCHEMA_EXISTING)
-          ? atob(localStorage.getItem(SCHEMA_EXISTING) as string)
-          : '',
-        localStorage.getItem(SCHEMA_PROPOSED)
-          ? atob(localStorage.getItem(SCHEMA_PROPOSED) as string)
-          : '',
-      ]}
+      value={[existing, proposed]}
       editorProps={{ $blockScrolling: true }}
       onChange={(value) => onEditorChange(value[0], value[1])}
+      onCursorChange={() => {
+        console.log('onCursorChange');
+      }}
     />
   );
 };
