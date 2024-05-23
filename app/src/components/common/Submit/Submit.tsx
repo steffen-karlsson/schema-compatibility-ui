c44dc12f4d3604ca5af9a0143a355877861f6f76
Steffen Karlsson <steffen.karlsson@maersk.com>
2024-05-23 09:35:17 +0200

    Better visibility validation on Validate button

--- app/src/components/common/Submit/Submit.tsx
+++ app/src/components/common/Submit/Submit.tsx
@@ -13,17 +13,21 @@ export interface SubmitProps {
 }
 
 const Submit: React.FC<SubmitProps> = ({ onValidateClick }) => {
+  const getDisabled = () => {
+    return (
+      !localStorage.getItem(SCHEMA_COMPATIBILITY_LEVEL) ||
+      !localStorage.getItem(SCHEMA_TYPE) ||
+      !localStorage.getItem(SCHEMA_EXISTING) ||
+      !localStorage.getItem(SCHEMA_PROPOSED)
+    );
+  };
+
   const [loading, setLoading] = React.useState(false);
-  const [disabled, setDisabled] = React.useState(true);
+  const [disabled, setDisabled] = React.useState(getDisabled());
 
   useEffect(() => {
     window.addEventListener('storage', () => {
-      setDisabled(
-        !localStorage.getItem(SCHEMA_COMPATIBILITY_LEVEL) ||
-          !localStorage.getItem(SCHEMA_TYPE) ||
-          !localStorage.getItem(SCHEMA_EXISTING) ||
-          !localStorage.getItem(SCHEMA_PROPOSED)
-      );
+      setDisabled(getDisabled());
     });
   }, []);
 
